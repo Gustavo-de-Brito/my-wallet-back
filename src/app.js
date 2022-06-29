d1b79b9f04a1e193df2654ef826dd4c0151c956a
Gustavo-de-Brito <gustavoaraujo086@gmail.com>
2022-06-29 11:08:30 -0300

    feat: valida os dados enviados no post em /clients

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express, { json } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { MongoClient } from "mongodb";
+import joi from "joi";
 
 dotenv.config();
 
@@ -21,6 +22,25 @@ mongoCLient.connect().then(() => {
 
 app.post("/clients", async (req, res) => {
   const newUser = req.body;
+
+  // password must have at least: 1 number, 1 lowercase character,
+  // 1 uppercase character, 1 special character, at least 8 characters
+  // at most 30 characters
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*]).{8,30}$/;
+
+  const userSchema = joi.object(
+    {
+      name: joi.string().required(),
+      email: joi.string().email().required(),
+      password: joi.string().pattern(passwordRegex)
+    }
+  );
+
+  const { error } = userSchema.validate(newUser);
+
+  if(error) {
+    return res.sendStatus(422);
+  }
 
   res.sendStatus(201);
 });
