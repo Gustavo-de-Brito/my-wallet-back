c424c874d3f1c2dd1ad281a2d474de1ec21f8ae2
Gustavo-de-Brito <gustavoaraujo086@gmail.com>
2022-07-02 12:02:56 -0300

    feat: retorna os dados de transacoes se existir algum

--- src/controllers/transactionsController.js
+++ src/controllers/transactionsController.js
@@ -1,3 +1,13 @@
-export function transactionsController(req, res) {
-  res.sendStatus(200);
+import { db, objectId } from "../databases/mongo.js";
+
+export async function transactionsController(req, res) {
+  const userId = res.locals.userId;
+
+  try {
+    const transactions = await db.collection("transactions").findOne({ userId: userId });
+
+    res.status(200).send(transactions ? transactions : {});
+  } catch{
+    res.sendStatus(500);
+  }
 }
--- src/middlewares/tokenValidation.js
+++ src/middlewares/tokenValidation.js
@@ -1,15 +1,17 @@
-import { db } from "../databases/mongo.js";
+import { db, objectId } from "../databases/mongo.js";
 
 async function tokenValidation(req, res, next) {
   const { token } = req.headers;
 
   try {
-    const isValidToken = await db.collection("sessions").findOne({token: token});
+    const validToken = await db.collection("sessions").findOne({token: token});
   
-    if(!isValidToken) {
+    if(!validToken) {
       return res.sendStatus(401);
     }
-  
+
+    res.locals.userId = objectId(validToken.userId);
+
     next();
   } catch(err) {
     res.sendStatus(500);
