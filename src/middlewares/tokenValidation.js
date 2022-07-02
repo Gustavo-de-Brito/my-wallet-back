afa17f9b930bd46377e9b3413ac5b4c2a049817e
Gustavo-de-Brito <gustavoaraujo086@gmail.com>
2022-07-02 12:51:25 -0300

    feat: atualiza a validacao do token

--- src/middlewares/tokenValidation.js
+++ src/middlewares/tokenValidation.js
@@ -1,11 +1,12 @@
 import { db, objectId } from "../databases/mongo.js";
 
 async function tokenValidation(req, res, next) {
-  const { token } = req.headers;
+  const { authorization } = req.headers;
+  const token = authorization?.replace("Bearer ", "");
 
   try {
-    const validToken = await db.collection("sessions").findOne({token: token});
-  
+    const validToken = await db.collection("sessions").findOne({ token });
+
     if(!validToken) {
       return res.sendStatus(401);
     }
